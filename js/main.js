1e86244e65077aceae1ac7a4dca7c52725c33bce
mohammed <mohammed7zaghloul@gmail.com>
2023-08-27 14:12:51 +0300

    Finaliz Menu And Logic

--- js/main.js
+++ js/main.js
@@ -378,4 +378,52 @@ document.querySelector(".reset-option").onclick = function () {
 
     // Relod Window
     window.location.reload();
+};
+
+// Toggle Menu 
+
+let toggleBtn = document.querySelector(".toggl-menu");
+
+let tLinks = document.querySelector(".links");
+
+toggleBtn.onclick = function (e) {
+
+    // Stop Propagation
+    e.stopPropagation();
+
+    // Toggle Class "menu-active" on Button
+    this.classList.toggle("menu-active");
+
+    // Toggle Class "open On Links"
+    tLinks.classList.toggle("open");
+};
+
+// Click Anywhere Outside Menu And Toggle Button
+
+document.addEventListener("click", (e) => {
+
+    if (e.target !== toggleBtn && e.target !== tLinks) {
+
+        // Check If Menu Is Open
+
+        if (tLinks.classList.contains("open")) {
+
+            // Toggle Class "menu-active" on Button
+            toggleBtn.classList.toggle("menu-active");
+
+            // Toggle Class "open On Links"
+            tLinks.classList.toggle("open");
+
+        }
+
+    }
+
+});
+
+// Stop Propagation On Menu
+
+tLinks.onclick = function (e) {
+
+    e.stopPropagation();
 }
+
